28cc6f3ab204b2928804e6fc242854f005f5f2b4
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-23 09:11:16 +0300

    data-ypes.js file is added

---
+++ data-ypes.js
@@ -0,0 +1,9 @@
+/* in here we will go to Data types
+    Undefined means something hasnt been defined ,
+    null means nothing,
+    boolean means true or false,
+    string means any sort of text,
+    symbol means,
+    number means is just anumber ,
+    object means store alot of keys,
+    */
