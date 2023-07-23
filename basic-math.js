4d3ba0e6ee7732a83fb6e85f4ac11350c06bcf7d
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-23 10:33:57 +0300

    basic-math.js file is added

---
+++ basic-math.js
@@ -0,0 +1,13 @@
+// Adding numbers
+var sum = 10 + 10;
+console.log(sum);
+
+// Subtracting numbers
+var difference = 45 - 30;
+
+// Multiplying Numbers
+var product = 8 * 8;
+
+// Dividing numbers
+
+var divide = 77 / 0;
