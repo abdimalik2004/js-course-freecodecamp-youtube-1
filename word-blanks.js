185f4106c90e6a033690f154ed81fa6330035742
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-24 23:06:32 +0300

    word-blanks.js file is added

---
+++ word-blanks.js
@@ -0,0 +1,9 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+    var result = "";
+    result += "The " + myAdjective + " " + myNoun + " " + myVerb + " " + "to the store " + myAdverb
+
+    return result;
+}
+
+console.log(wordBlanks("dog", "big", "ran", "quickly"));
+console.log(wordBlanks("bike", "slow", "flew", "slowly"));
