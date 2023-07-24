5af02645476861d4beecfadf9715338d43e05f7f
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-24 12:32:01 +0300

    augmented-math-operation.js file is added

---
+++ augmented-math-operation.js
@@ -0,0 +1,35 @@
+// Adding 
+var a = 3;
+var b = 17;
+var c = 12;
+
+a += 12;
+b += b;
+c += 7;
+
+// Subt
+var a = 11;
+var b = 9;
+var c = 3;
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+// product
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+// divide 
+var a = 48;
+var b = 108;
+var c = 33;
+
+a /= 12;
+b /= 4;
+c /= 11;
