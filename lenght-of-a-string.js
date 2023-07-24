521a6753521399d5bcce881a089eb6d1e1a56d7a
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-24 12:34:29 +0300

    lenght-of-a-string.js file is added

---
+++ lenght-of-a-string.js
@@ -0,0 +1,5 @@
+//How to find the lenght of a string using The length command
+var fristNamelength = 0;
+var fristANme = "Abdimalik";
+fristNamelength = fristANme.length;
+console.log(fristNamelength)
