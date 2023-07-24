6010089e8a4e3a1f6db2ae324ccd686ab8484ee4
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-24 23:05:56 +0300

    find-the-nth-character.js file is added

---
+++ find-the-nth-character.js
@@ -0,0 +1,18 @@
+var lastName = "Abdirahman" ;
+var thirdLetterOfLastName = lastName[2];
+console.log(lastName)
+
+// you can also use bracket notation to find last character in string
+var fristName = "abdi";
+var lastLetterOfFristName = fristName[fristName.length - 1]
+
+var lastName = "Abdirahman";
+
+var lastLetterOfLastName = lastName[lastName.length-1];
+// br notation to find nth to last character in sring
+var fristName = "Abdimalik";
+var thirdLetterOfFristName = fristName[fristName.length-3];
+
+var lastName = "Abdirahman";
+
+var secondToLastLetterOfLastName = lastName[lastName.length -3];
