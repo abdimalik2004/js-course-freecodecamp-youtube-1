f2c4b7e4574d7a27ff7678059417c8bedd550fa0
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-24 12:36:02 +0300

    plus-equals-operator.js file is added

---
+++ plus-equals-operator.js
@@ -0,0 +1,7 @@
+// Also you can concanite strings using += operators
+var ourStr = "I come frist. ";
+ourStr += "I come second. ";
+// tetsing
+var mystr = "This is the frist sentence. " ;
+mystr += "This is the second sentence. " ;
+console.log(mystr);
