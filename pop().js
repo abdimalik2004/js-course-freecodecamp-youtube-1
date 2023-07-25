cf850e08ab268358ee3a8eec6f5f5c8dbad59661
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-25 11:24:11 +0300

    pop().js file is added

---
+++ pop().js
@@ -0,0 +1,9 @@
+// manipulate Arrays whith pop()
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+
+// setup
+var myArray = [["Abdimalik",19], ["cat", 2]];
+
+var removedFromMyArray = myArray.pop();
+console.log(myArray)
