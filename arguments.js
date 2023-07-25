87169e2fa79b50e91b2794babb5a6c5d3b0db108
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-25 12:18:52 +0300

    arguments.js file is added

---
+++ arguments.js
@@ -0,0 +1,11 @@
+// Function whith Arg it will do in wich operation you want
+function ourfunctionWhithArgs(a, b) {
+    console.log(a + b);
+}
+
+ourfunctionWhithArgs(10,5);
+
+function myFunctionWhithArgs(a, b) {
+    console.log(a * b);
+}
+myFunctionWhithArgs(12, 12);
