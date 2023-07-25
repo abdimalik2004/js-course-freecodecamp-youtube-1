8bdea22dbb85d7e948df5c838d025836e8453fdf
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-25 12:19:35 +0300

    global-scobe.js file is added

---
+++ global-scobe.js
@@ -0,0 +1,20 @@
+var myGlobal = 10;
+
+
+function fun1() {
+    oopsMyGlobal = 5;
+}
+
+
+function fun2() {
+    var output = "";
+    if (typeof myGlobal != "undefined") {
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsMyGlobal != "undefined") {
+    output += "oopsMyGlobal: " + oopsMyGlobal;
+    }
+    console.log(output);
+}
+fun1();
+fun2();
