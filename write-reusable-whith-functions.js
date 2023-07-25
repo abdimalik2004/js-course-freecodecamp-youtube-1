3f7ec29615659953e19e66998df74f9597b6f622
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-25 12:21:30 +0300

    write-reusable-whith-functions.js file is added

---
+++ write-reusable-whith-functions.js
@@ -0,0 +1,11 @@
+function ourReusableFunction() {
+    console.log("Heyya, world");
+}
+
+ourReusableFunction();
+
+//setup
+function reusableFunction() {
+    console.log("Hello world");
+}
+reusableFunction();
