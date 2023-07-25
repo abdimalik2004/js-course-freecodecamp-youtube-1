75582c176691e117e69067c8ac46476fc7eec8e6
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-25 11:32:28 +0300

    shift().js file is added

---
+++ shift().js
@@ -0,0 +1,8 @@
+//manipulate Arrays whith shift
+var ourArray = ["Stimpson", "A", ["cat"]];
+var removedFromOurArray = ourArray.shift();
+
+//setup
+var myArray = [["Abdimalik",19], ["dog", 4]];
+var removedFromMyArray = myArray.shift();
+console.log(myArray);
