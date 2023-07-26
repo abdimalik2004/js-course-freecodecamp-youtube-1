2cbbbaf95000a68e7ccdf313df20804bc6861f5c
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-26 12:19:02 +0300

    and-or-operators.js file is added

---
+++ and-or-operators.js
@@ -0,0 +1,16 @@
+function testLogicalAnd(val) {
+    if (val <= 50 && val >=25) {
+        return"Yes";
+    }
+    return "No";
+}
+console.log(testLogicalAnd(10)); // Output No
+// Comparisons with The Logical Or Operator
+function testLogicalOr(val) {
+    if (val < 10  || val >20 ) {
+        return "Outside";
+    }
+    return "Inside";
+}
+
+console.log(testLogicalOr(15)); // Output Inside
