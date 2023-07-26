ba8351d1337d3a9aa37032ec0c10c1d15e9799df
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-26 10:17:11 +0300

    stand-in-line.js file is added

---
+++ stand-in-line.js
@@ -0,0 +1,10 @@
+function nextInline(arr, item) {
+    arr.push(item);
+
+    return arr.shift();
+}
+
+var testArr = [1,2,3,4,5];
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInline(testArr, 6));
+console.log("Afte: " + JSON.stringify(testArr));
