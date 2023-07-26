92b94f42821a48f41a64efd387c3dd4248b1d104
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-26 12:19:59 +0300

    else-if-statements.js file is added

---
+++ else-if-statements.js
@@ -0,0 +1,11 @@
+function testElseIf(val) {
+    if (val > 10) {
+        return "Greater Than 10";
+    } else if (val < 5) {
+        return "Samller Than 5";
+    } else {
+        return "Between 5 and 10";
+    } 
+}
+
+console.log(testElseIf(7)); // Output Between 5 and 10
