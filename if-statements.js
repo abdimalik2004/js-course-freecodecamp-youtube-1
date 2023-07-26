7bcb0e286a9c13739f5690785e89403a277ff287
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-26 12:21:37 +0300

    if-statements.js file is added

---
+++ if-statements.js
@@ -0,0 +1,16 @@
+function ourTrueOrFalse(isItTrue) {
+    if (isItTrue) {
+        return "yes, it's true";
+    }
+    return "not it's not true";
+}
+
+function trueOrFalse(wasThatTrue) {
+    if (wasThatTrue) {
+        return "yes, that was true";
+    }
+    return "no that was false";
+
+}
+
+console.log(trueOrFalse(true));// Output yes, that was true
