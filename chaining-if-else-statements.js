6d8a107d737f46d9c3042dc8b86990274f266302
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-26 12:19:29 +0300

    chaining-if-else-statements.js file is added

---
+++ chaining-if-else-statements.js
@@ -0,0 +1,15 @@
+function testSize(num) {
+    if (num < 5) {
+        return "Yinny";
+    } else if (num < 10) {
+        return "Small";
+    } else if (num < 15) {
+        return "Medium";
+    } else if (num < 20) {
+        return "large";
+    } else {
+        return "Huge";
+    }
+}
+
+console.log(testSize(7)); // Output Small
