10241fdad0f145c5ac9c088d8fadc6654f6bdd6b
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-26 10:15:47 +0300

    assigment-with-a-returned-value.js file is added

---
+++ assigment-with-a-returned-value.js
@@ -0,0 +1,16 @@
+var changed = 0;
+
+function change(sum) {
+    return (num + 5) / 3;
+}
+
+changed = changed(10);
+
+var processed = 0;
+function processedArg(num) {
+    return (num + 3) / 5;
+}
+
+processed = processedArg(7);
+
+console.log(processed)// output is 2
