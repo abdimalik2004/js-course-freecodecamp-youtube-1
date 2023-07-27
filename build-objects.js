473061b1e8e3f9ddceb84d77a2261b99c3b72087
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-27 12:31:14 +0300

    build-objects.js filr is added

---
+++ build-objects.js
@@ -0,0 +1,13 @@
+var ourDog = {
+  "name": "Camper",
+  "Legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+var myDog = {
+  "name": "Camper",
+  "Legs": 3,
+  "tails": 2,
+  "friends": []
+}
