aa98fd7425542c4a4c54638c9e923401df153898
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-27 12:35:36 +0300

    objects-for-lookups.js file is added

---
+++ objects-for-lookups.js
@@ -0,0 +1,20 @@
+// Setup
+function phoneticLookup(val) {
+    let result = "";
+  
+    // Only change code below this line
+    var lookups = {
+      "alpha": "Adams",
+      "bravo": "Boston",
+      "charlie": "Chicago",
+      "delta": "Denver",
+      "echo": "Easy",
+      "foxtrot": "Frank",
+    };
+  
+    // Only change code above this line
+    result = lookups[val];
+    return result;
+}
+  
+console.log(phoneticLookup("charlie"));
