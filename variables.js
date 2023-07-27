eee908744cd11e693c8f767657b052acddb20e9b
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-27 12:41:00 +0300

    variables.js file is added

---
+++ variables.js
@@ -0,0 +1,11 @@
+// Setup
+const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+};
+  
+// Only change code below this line
+var playerNumber = 12;  // Change this line
+var player = testObj[playerNumber];   // Change this line
+console.log(player)
