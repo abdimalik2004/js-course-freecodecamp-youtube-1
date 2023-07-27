a217df971a19e3e342dde6b89c7c8c78a7706ebb
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-27 12:37:15 +0300

    return-early-pattern-for-functions.js file is added

---
+++ return-early-pattern-for-functions.js
@@ -0,0 +1,9 @@
+// Setup
+function abTest(a, b) {
+  if (a < 0 || b < 0) {
+    return undefined;
+  }
+  
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+console.log(abTest(2,2)); // Output 8
