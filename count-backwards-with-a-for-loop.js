595c7ce9100790b31eff2699aa2ba9052cb885ac
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-27 18:10:03 +0300

    count-backwards-with-a-for-loop.js file is added

---
+++ count-backwards-with-a-for-loop.js
@@ -0,0 +1,15 @@
+const ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+console.log(ourArray);
+
+// Setup
+const myArray = [];
+for (var i = 9; i > 0; i -= 2) {
+    myArray.push(i)
+}
+console.log(myArray)
+
+// Only change code below this line
