02897da150fb70a61e0e18380aa0af3a8039b36f
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-27 18:46:36 +0300

    nasted-objects.js filre is added

---
+++ nasted-objects.js
@@ -0,0 +1,26 @@
+const ourStorage = {
+    "desk": {
+      "drawer": "stapler"
+    },
+    "cabinet": {
+      "top drawer": { 
+        "folder1": "a file",
+        "folder2": "secrets"
+      },
+      "bottom drawer": "soda"
+    }
+};
+var myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+};
+  
+var gloveBoxContents = myStorage.car.inside["glove box"];
+console.log(gloveBoxContents);
