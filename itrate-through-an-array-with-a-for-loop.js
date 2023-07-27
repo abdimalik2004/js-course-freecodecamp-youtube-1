b97da9020278fd0bc27db0ec4bd76b9e594ece17
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-27 18:11:26 +0300

    itrate-through-an-array-with-a-for-loop.js file is added

---
+++ itrate-through-an-array-with-a-for-loop.js
@@ -0,0 +1,16 @@
+var arr = [9, 10, 11, 12];
+var ourTotal = 0;
+for (var i = 0; i < arr.length; i++) {
+    ourTotal += ourTotal[i];
+}
+console.log(ourTotal);
+
+// Setup
+const myArray = [2, 3, 4, 5, 6];
+var total = 0;
+for (var i = 0; i < myArray.length; i++) {
+    total += myArray[i];
+}
+console.log(total)
+
+// Only change code below this line
