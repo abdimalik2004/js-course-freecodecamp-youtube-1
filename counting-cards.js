56c9722fb58ca9c41759b2b18397ca1d746b5be5
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-27 12:32:51 +0300

    counting-cards.js file is added

---
+++ counting-cards.js
@@ -0,0 +1,30 @@
+var count = 0;
+
+function cc(card) {
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+
+  var holdbet = 'Hold'
+  if (count > 0) {
+    holdbet = 'Bet'
+  }
+
+  return count + " " + holdbet;
+}
+
+cc(2); cc('K'); cc(10); cc('K'); cc('A');
+console.log(cc(4));
