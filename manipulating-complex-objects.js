926acce17ad1e16b7b73638c6281a21c7c98c85a
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-27 12:35:06 +0300

    manipulating-complex-objects.js file is added

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,21 @@
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+    {
+      "artist": "Abdi",
+      "title": "Gaad",
+      "Relsease year": 2003,
+      "Formats": [
+        "Youtube videos"
+      ]
+    }
+  ];
