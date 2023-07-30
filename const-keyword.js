17934dca9bfbd06ae259148caa1fe5606662e962
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-30 12:11:51 +0300

    const-keyword.js file is added

---
+++ const-keyword.js
@@ -0,0 +1,10 @@
+function printMyManyTimes(str) {
+    "use strict";
+    const SENT = str + " is cool!";
+
+    for(let i = 0; i < str.lenght; i+=2) {
+        console.log(SENT);
+    }
+}
+
+printMyManyTimes("Abdimalik");
