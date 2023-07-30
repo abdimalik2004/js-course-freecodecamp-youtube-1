b93c9071caa67e6de191074dcaa9e1cdc67ccd98
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-30 12:15:57 +0300

    random-fraction-and-whole-numbers.js file is added

---
+++ random-fraction-and-whole-numbers.js
@@ -0,0 +1,22 @@
+function randomFraction() {
+
+  
+    return Math.random();
+}
+console.log(randomFraction());
+
+
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+function randomWholeNum() {
+    return Math.floor(Math.random() * 10);
+}
+console.log(randomWholeNum());
+
+
+
+function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+}
+var myRandom = randomRange(5, 15);
+
+console.log(myRandom);
