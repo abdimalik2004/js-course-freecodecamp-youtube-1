4209c017fada474e8e310c685cf961666447b9f0
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-30 12:13:51 +0300

    preseint-function.js file is added

---
+++ preseint-function.js
@@ -0,0 +1,14 @@
+function convertToInteger(str) {
+    return parseInt(str);
+}
+
+console.log(convertToInteger("56"));
+
+
+
+
+function convertToInteger(str) {
+    return parseInt(str, 4)
+}
+
+console.log(convertToInteger("10011"));
