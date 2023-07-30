d644988d5b6ce992e585bd3596c311806344fd79
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-30 12:11:19 +0300

    arrow-functions.js file is added

---
+++ arrow-functions.js
@@ -0,0 +1,19 @@
+const magic = () => new Date();
+
+
+
+// Write Arrow Functions with Paramaters 
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+  
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+//write Higher Order Arrow Functions
+const realMumberArray = [4,5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squarList = (arr) => {
+    const squareIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x);
+    return squareIntegers;
+};
+
+
+const squareIntegers = squarList(realMumberArray);
+console.log(squareIntegers);
