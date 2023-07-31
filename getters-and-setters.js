bc25c19f5805d4b788a0c5b8e8a8960ebdc2e9c7
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-31 15:09:46 +0300

    getters-and-setters.js file is added

---
+++ getters-and-setters.js
@@ -0,0 +1,33 @@
+class Book {
+    constructor(author) {
+        this._author = author;
+    }
+    get writer() {
+        return this._author;
+    }
+
+    set writer(updatedAuthor){
+        this._author = updatedAuthor;
+    }
+}
+function makeClass() {
+    class Thermostat {
+        constructor(temp) {
+            this._temp = 5/9 * (temp - 32);
+        }
+        get temprature(){
+            return this._temp;
+        }
+        set temprature(updatedTemp){
+            this._temp = updatedTemp
+        }
+    }
+    return Thermostat;
+}
+const Thermostat = makeClass();
+const thermos = new Thermostat(76);
+let temp = thermos.temprature;
+thermos.temprature = 26;
+temp = thermos.temprature;
+
+console.log(temp);
