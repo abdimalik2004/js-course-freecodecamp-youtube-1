465767b4448d68a684a8424a4129d9c5b2acae7d
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-07-31 15:07:24 +0300

    class-syntax.js file is added

---
+++ class-syntax.js
@@ -0,0 +1,21 @@
+class SpaceShuttle{
+    constructor(targwtPalnet) {
+        this.targwtPalnet = targwtPalnet;
+    }
+}
+var zeus = new SpaceShuttle('Jupiter');
+
+console.log(zeus.targwtPalnet);
+
+
+function makeClass() {
+    class Vegatable {
+        constructor (name){
+            this.name = name;
+        }
+    }
+    return Vegatable;
+}
+const Vegatable = makeClass();
+const carrot = new Vegatable('carrot');
+console.log(carrot.name);
