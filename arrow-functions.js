7dda45b9b1ad6d76b12cca849f3a1ecf5abeadbe
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-08-01 15:30:18 +0300

    arrow-functions.js file is added

--- arrow-functions.js
+++ arrow-functions.js
@@ -8,7 +8,7 @@ const myConcat = (arr1, arr2) => arr1.concat(arr2);
 console.log(myConcat([1, 2], [3, 4, 5]));
 
 //write Higher Order Arrow Functions
-const realMumberArray = [4,5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const realMumberArray = [4,5.6, -9.8, 3.14, 42, 6, 8.35, -2];
 const squarList = (arr) => {
     const squareIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x);
     return squareIntegers;
