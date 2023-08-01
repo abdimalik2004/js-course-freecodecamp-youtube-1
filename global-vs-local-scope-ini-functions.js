28244592d683c65848558667c21da409397af404
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-08-01 23:16:34 +0300

    global-vs-local-scope-ini-functions.js file is added

--- global-vs-local-scope-ini-functions.js
+++ global-vs-local-scope-ini-functions.js
@@ -1,9 +1,10 @@
-// local scope have more power than global when consoling
+//local scope have more power than global when consoling
 var outWear = "T-Shirt";
 function myOutfit() {
     var outWear = "sweater"
 
+    
     return outWear;
-}
+};
 console.log(myOutfit());
 console.log(outWear);
