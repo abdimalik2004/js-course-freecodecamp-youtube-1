be98827d1d7808b266756aa5f58ad6b7258a5ccc
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-08-01 22:55:37 +0300

    declare-variable.js file is added

--- declare-variable.js
+++ declare-variable.js
@@ -1,11 +1,9 @@
-   /* how to declre a viriable*/
-   var myName = "abdimalik"
-   /* var will be used the while program */
+/* how to declre a viriable*/
+var myName = "abdimalik";
+/* var will be used the while program */
 
-   myNuber = 10
-   /* but let will only be used whith the scope of where you decalred */
-   let ourName = "abdimalik"
-   
-
-   /* const variable won't changed if you try you will get an error */
-   const pi = 3.14
+myNuber = 10;
+/* but let will only be used whith the scope of where you decalred */
+let ourName = "abdimalik";   
+/* const variable won't changed if you try you will get an error */
+const pi = 3.14;
