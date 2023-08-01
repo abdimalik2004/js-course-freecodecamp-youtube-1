d1112da1e1f9a0ede6da8b7891eb251e22eb4aeb
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-08-01 23:49:12 +0300

    stand-in-line.js file is added

--- stand-in-line.js
+++ stand-in-line.js
@@ -7,4 +7,4 @@ function nextInline(arr, item) {
 var testArr = [1,2,3,4,5];
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInline(testArr, 6));
-console.log("Afte: " + JSON.stringify(testArr));
+console.log("Afte: " + JSON.stringify(testArr)); 
