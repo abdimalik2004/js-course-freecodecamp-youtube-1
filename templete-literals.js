f42d9ac7c1ffabb758714325d3bca1aacff4d617
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-08-03 17:44:39 +0300

    templete-literals.js file is updted

--- templete-literals.js
+++ templete-literals.js
@@ -5,9 +5,7 @@ const person = {
 const greeting =  `Hello, my name is ${person.name}!
 I am ${person.age} years old.` ;
 
-console.log(greeting);
-
-
+console.log(greeting); 
 
 const result = {
     success: ["max-length", "no-amd", "prefer-arrow-functions"],
