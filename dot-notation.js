75cc6a91ce7f2d877e20c68e071de15f9bcd0316
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-08-03 15:21:28 +0300

    dot-notation.js file is updated

--- dot-notation.js
+++ dot-notation.js
@@ -7,4 +7,4 @@ const testObj = {
 // Only change code below this line
 var hatValue = testObj.hat;      // Change this line
 var shirtValue = testObj.shirt;    // Change this line
-console.log(shirtValue);
+console.log(shirtValue); 
