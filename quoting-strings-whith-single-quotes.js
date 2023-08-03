bb1a402d49d17386244fa3e4645d4a999f7f6507
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-08-03 17:40:03 +0300

    quoting-strings-whith-single-quotes.js file is updted

--- quoting-strings-whith-single-quotes.js
+++ quoting-strings-whith-single-quotes.js
@@ -1,2 +1,2 @@
 // in case of using (\) as a escape character you can use a single quotos
-var myStr = '<a href="https://www.examle.com" target="_blank">Link</a>' ;
+var myStr = '<a href="https://www.examle.com" target="_blank">Link</a>'; 
