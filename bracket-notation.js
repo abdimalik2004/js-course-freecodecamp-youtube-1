840937147c1dcee3b5661aed5b0fb17ae6c1d534
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-08-12 18:06:49 +0300

    updated

--- bracket-notation.js
+++ bracket-notation.js
@@ -1,4 +1,4 @@
- var firstLetterOffirstName = "";
- var lastName = "Abdirahman";
- firstLetterOffirstName = lastName[0];
- console.log(firstLetterOffirstName);
+var firstLetterOffirstName = "";
+var lastName = "Abdirahman";
+firstLetterOffirstName = lastName[0];
+console.log(firstLetterOffirstName);
