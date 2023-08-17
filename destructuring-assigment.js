fa404f5490fb9abc0bc6c534cf6a91d195c8e0a1
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-08-17 15:22:31 +0300

    updated

--- destructuring-assigment.js
+++ destructuring-assigment.js
@@ -19,7 +19,6 @@ function getTempOFTmrw(avgTemperatures) {
 console.log(getTempOFTmrw(AVG_TEMPERATURES));
 
 //Destructing Assigment with Nested Objects
-
 const LOCAL_FORECAST = {
     today: { min: 64, max: 77 },
     tomorrow: { min: 68, max: 80 }
