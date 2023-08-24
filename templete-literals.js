1828d78fc9b47a7b694014485a322cd75aa5304b
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-08-24 23:28:04 +0300

    updated

--- templete-literals.js
+++ templete-literals.js
@@ -11,7 +11,7 @@ const result = {
     success: ["max-length", "no-amd", "prefer-arrow-functions"],
     failure: ["no-var", "var-on-top", "linebreak"],
     skipped: ["no-extra-semi", "no-dup-keys"]
-}; 
+};
 function makeList(arr) {
     const resultDisplayArray = [];
     for (let i = 0; i < arr.length; i++) {
