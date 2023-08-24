26b45f7f67f480fbf005c97ebcc24cfdd6291279
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-08-24 23:16:25 +0300

    updated

--- declare-variable.js
+++ declare-variable.js
@@ -6,4 +6,4 @@ myNuber = 10;
 /* but let will only be used whith the scope of where you decalred */
 let ourName = "abdimalik";   
 /* const variable won't changed if you try you will get an error */
-const PI = 3.14;  
+const PI = 3.14;
