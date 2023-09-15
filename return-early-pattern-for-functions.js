b9e86b790d7511e1f431c0c7bd1ca77a156dba55
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-09-15 11:12:23 +0300

    updated

--- return-early-pattern-for-functions.js
+++ return-early-pattern-for-functions.js
@@ -3,7 +3,7 @@ function abTest(a, b) {
   if (a < 0 || b < 0) {
     return undefined;
   }
-  
-    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
-console.log(abTest(2,2)); //Output 8 
+console.log(abTest(2, 2)); //Output 8
