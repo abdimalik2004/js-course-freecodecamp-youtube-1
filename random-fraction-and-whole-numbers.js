78c4a18909d1809aead11853480b12f377438403
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-09-15 11:10:53 +0300

    updated

--- random-fraction-and-whole-numbers.js
+++ random-fraction-and-whole-numbers.js
@@ -16,4 +16,4 @@ function randomRange(myMin, myMax) {
     return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
 }
 var myRandom = randomRange(5, 15);
-console.log(myRandom);
+console.log(myRandom); 
