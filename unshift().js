10693796a845babb8b541680eea64aa8a722dfef
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-09-15 11:16:07 +0300

    updated

--- unshift().js
+++ unshift().js
@@ -3,7 +3,10 @@ var ourArray = ["Stimpson", "A", ["cat"]];
 var removedFromOurArray = ourArray.shift();
 ourArray.unshift("HAppy");
 //setup
-var myArray = [["Abdimalik",19], ["dog", 4]];
+var myArray = [
+  ["Abdimalik", 19],
+  ["dog", 4],
+];
 var removedFromMyArray = myArray.shift();
 myArray.unshift("Abdimalik", 18);
 console.log(myArray);
