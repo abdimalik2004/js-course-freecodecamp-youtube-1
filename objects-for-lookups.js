f123790cb180e372f1d5e5cd4645f642c29d8f41
abdimalik2004 <mahadalleabdimalik@gmail.com>
2023-09-15 11:06:54 +0300

    updated

--- objects-for-lookups.js
+++ objects-for-lookups.js
@@ -1,19 +1,19 @@
 // Setup
 function phoneticLookup(val) {
-    let result = "";
-  
-    // Only change code below this line
-    var lookups = {
-      "alpha": "Adams",
-      "bravo": "Boston",
-      "charlie": "Chicago",
-      "delta": "Denver",
-      "echo": "Easy",
-      "foxtrot": "Frank",
-    };
-  
-    // Only change code above this line
-    result = lookups[val];
-    return result;
+  let result = "";
+
+  // Only change code below this line
+  var lookups = {
+    alpha: "Adams",
+    bravo: "Boston",
+    charlie: "Chicago",
+    delta: "Denver",
+    echo: "Easy",
+    foxtrot: "Frank",
+  };
+
+  // Only change code above this line
+  result = lookups[val];
+  return result;
 }
 console.log(phoneticLookup("charlie"));
